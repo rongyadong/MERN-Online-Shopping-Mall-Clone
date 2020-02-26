8b06ee63036b893d54c17dccc2b8905e6d390d52
JackRong <rongyadong19900724@gmail.com>
2020-02-26 22:26:55 +1100

    added a function by using moment.js to convert the timestamp to local time record in the history page

--- client/src/components/views/HistoryPage/HistoryPage.js
+++ client/src/components/views/HistoryPage/HistoryPage.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Axios from "axios";
+import moment from "moment";
 
 const HistoryPage = () => {
 
@@ -21,7 +22,7 @@ const HistoryPage = () => {
             <td>{item.paymentId}</td>
             <td>{item.price}</td>
             <td>{item.quantity}</td>
-            <td>{item.dateOfPurchase}</td>
+            <td>{moment.utc(item.dateOfPurchase).format("DD / MM / YYYY")}</td>
         </tr>
     ));
 
